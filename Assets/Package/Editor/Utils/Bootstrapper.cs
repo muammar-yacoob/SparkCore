113b988fb5c65c834361b6f797e3ea086828036f
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2022-11-20 15:34:54 +0000

    Debugger added with Bootstrapper

---
+++ Assets/Package/Editor/Utils/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SparkCore.Editor.Utils
+{
+    public static class Bootstrapper
+    {
+#if UNITY_EDITOR
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Startup()
+        {
+            Debug.Log($"Starting up from {nameof(Bootstrapper)}");
+            var debugger = Resources.Load("Debugger");
+            if(debugger == null) return;
+            Object.DontDestroyOnLoad((Object.Instantiate(debugger)));
+        }
+#endif
+    }
+}
--- Assets/Package/Runtime/Utils/Debugger.cs
+++ Assets/Package/Runtime/Utils/Debugger.cs
@@ -1,27 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace SparkCore.Editor.Utils
+namespace SparkCore.Runtime.Utils
 {
     public class Debugger : MonoBehaviour
     {
-#if UNITY_EDITOR
         private void Awake()
         {
+#if UNITY_EDITOR
             SceneManager.LoadScene(0);
             ClearConsole();
             Debug.Log($"{nameof(Debugger)} instantiated", this);
-        }
 #endif
+        }
         
-#if UNITY_EDITOR
         private static void ClearConsole()
         {
+#if UNITY_EDITOR
             var logEntries = System.Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
             var clearMethod = logEntries.GetMethod("Clear",
                 System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
             clearMethod.Invoke(null, null);
-        }
 #endif
+        }
     }
 }
