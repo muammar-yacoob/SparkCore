852d6d91f59e1205853616254077679ffecb56c1
Muammar Yacoob <muammar@born.net>
2023-04-11 15:32:20 +0100

    Highlight Prefabs With Missing Scripts

--- Assets/Package/Editor/Menus/ToolsMenu.cs
+++ Assets/Package/Editor/Menus/ToolsMenu.cs
@@ -21,6 +21,11 @@ namespace SparkCore.Editor.Menus
         [MenuItem("Tools/Setup/Unity Packages/Cinemachine", false, 33)]
         public static void InstallUnity_Cinemachine() => Packages.InstallUnityPackage("cinemachine");
 
+        [MenuItem("Tools/Utils/Find Missing Scripts in project", false, 51)]
+        public static void Find_Prefabs_Missing_Scripts_In_Project() => Utils.PrefabsWithMissingScripts.FindInProject();
+        [MenuItem("Tools/Utils/Find Missing Scripts in Hierarchy", false, 52)]
+        public static void Find_Objects_Missing_Scripts_In_Hierarchy() => Utils.PrefabsWithMissingScripts.FindInHierarchy();
+
         [MenuItem("Tools/About", false, 1011)]
         public static void MenuHelp() => Application.OpenURL($"https://github.com/muammar-yacoob/SparkCore");
     }
