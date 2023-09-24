b5c8616f28cbbe517da3972d303b07600c9a10be
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2023-09-24 18:35:12 +0100

    prefabs with missing scripts tools menu fixed

--- Assets/Package/Editor/Menus/ToolsMenu.cs
+++ Assets/Package/Editor/Menus/ToolsMenu.cs
@@ -1,4 +1,5 @@
 using SparkCore.Editor.Setup;
+using SparkCore.Editor.Utils;
 using UnityEditor;
 using UnityEngine;
 
@@ -20,6 +21,12 @@ namespace SparkCore.Editor.Menus
         
         [MenuItem("Spark Core/Setup/Unity Packages/Cinemachine",false,33)]
         public static void InstallUnity_Cinemachine() => Packages.Install("com.unity.cinemachine");
+
+        [MenuItem("Tools/Find missing scripts in Hierarchy", false, 41)]
+        public static void FindMissingInHierarchy() => PrefabsWithMissingScripts.FindInHierarchy();
+
+        [MenuItem("Tools/Find missing scripts in Project", false, 42)]
+        public static void FindMissingInProject() => PrefabsWithMissingScripts.FindInProject();
 
         [MenuItem("Spark Core/About", false, 1011)]
         public static void MenuHelp() => Application.OpenURL($"https://github.com/Born-Studios/BornCore");
