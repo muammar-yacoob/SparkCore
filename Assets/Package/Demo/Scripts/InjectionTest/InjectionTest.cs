c92c951392075feb84bd9f9da45c5b81d1a1bd4a
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-11 10:53:50 +0000

    Custom [Inject] attribute added

--- Assets/Package/Demo/Scripts/InjectionTest/InjectionTest.cs
+++ Assets/Package/Demo/Scripts/InjectionTest/InjectionTest.cs
@@ -1,11 +1,10 @@
 using SparkCore.Runtime.Core;
-using VContainer;
-
+using SparkCore.Runtime.Injection;
 namespace SparkDev.Demo.InjectionTest
 {
     public class InjectionTest : InjectableMonoBehaviour
     {
-        [Inject] private ILogger logger;
+        [Inject(typeof(ConsoleLogger))] private ILogger logger;
 
         private void OnEnable()
         {
