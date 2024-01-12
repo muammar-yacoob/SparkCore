be5d1805c798293833f6dbfb13a49ec2e1c6615f
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-12 22:55:14 +0000

    Reflection helpers moved to InjectionExtensions

---
+++ Assets/Package/Runtime/Extensions/InjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SparkCore.Runtime.Utils
+{
+    public class InjectionExtensions
+    {
+        //Helpers
+        public static Assembly GetDefualtAssembly() => GetAssemblyByName("Assembly-CSharp");
+
+        static Assembly GetAssemblyByName(string name) => AppDomain.CurrentDomain.GetAssemblies()
+            .FirstOrDefault(assembly => assembly.GetName().Name == name);
+
+        static IEnumerable<Type> GetTypesInDefaultAssembly<T>() => GetDefualtAssembly().GetTypes()
+            .Where(type => typeof(T).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract);
+    }
+}
